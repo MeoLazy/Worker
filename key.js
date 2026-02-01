65232162447d79b00eb12212f4e1ee473768e222
this is meo lazy <meowxcutiz@gmail.com>
2026-02-01 22:09:43 +0700

    Add files via upload

--- key.js
+++ key.js
@@ -90,6 +90,13 @@ export default {
   }
 };
 
+
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization"
+};
+
 // ================= HELPERS =================
 
 function generateKey(len) {
